c37f445a00c13eceb6dbc18deba5d99756d9e02e
Dorian Vinai <dorian.vinai@epita.fr>
2020-12-09 02:31:08 +0100

    Renamed mail

--- osmapi_test.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python
-
-from osmapi import OsmApi
-
-MyApi = OsmApi(username = u"coulona28@gmail.com", password = u"Croquedinde94")
-MyApi.ChangesetCreate({u"comment": u"My first test"})
-print(MyApi.NodeCreate({u"lon":1, u"lat":1, u"tag": {}}))
-MyApi.ChangesetClose()
